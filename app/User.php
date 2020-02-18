1f688f0229f52086d92ca25a98e53b3a79789d1a
babak <babakus7@gmail.com>
2020-02-18 19:49:00 +0330

    User Model done

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
@@ -17,6 +17,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name', 'email', 'password',
+        'api_token', 'user_type', 'liked_posts',
+        'favourite_categories', 'favourite_posts', 'disliked_posts',
+        'preferences', 'avatar'
     ];
 
     /**
